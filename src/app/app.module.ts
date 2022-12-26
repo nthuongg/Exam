c9cfd447683a1ee3b8414d1eb6c366926a0de995
nthuongg <nguyenhuongg1104@gmail.com>
2022-12-26 10:01:36 +0700

    huongxinh

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {RouterModule, Routes} from "@angular/router";
 import { AppComponent } from './app.component';
+import {BaithiComponent} from "./baithi/baithi.component";
+const appRoutes: Routes = [
 
+  {path:'noidung1',component:BaithiComponent},
+
+
+];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,BaithiComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/baithi/baithi.component.ts
@@ -0,0 +1,18 @@
+import {Component} from "@angular/core";
+
+@Component({
+
+  selector:'baithi',
+  templateUrl:'./baithi.component.html'
+})
+
+export class BaithiComponent{
+
+  noidung:any =[
+    {name:'Gemma Roberson',Dc1:'Apology for late respone email',Text:'Hello Colette Wooten,'},
+    {name:'Ann Garza',Dc1:'Application for Job Title',Text:'Hello Colette Wooten,'},
+    {name:'Alfonso Burnett',Dc1:'Anything I can help with',Text:'Hello Colette Wooten,'},
+    {name:'Rogan Espinoza',Dc1:'Assistant Marketing Department',Text:'Hello Colette Wooten,'},
+    {name:'Sierra Kerr',Dc1:'Application for Transfer',Text:'Hello Colette Wooten,'}
+  ];
+}
